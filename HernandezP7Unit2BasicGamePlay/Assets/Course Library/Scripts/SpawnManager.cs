006ddd2ebb3ad2eca40e51638c385937829f2d0f
718355 <145615429+718355@users.noreply.github.com>
2024-03-26 17:57:32 -0700

    Not meant for main

--- HernandezP7Unit2BasicGamePlay/Assets/Course Library/Scripts/SpawnManager.cs
+++ HernandezP7Unit2BasicGamePlay/Assets/Course Library/Scripts/SpawnManager.cs
@@ -40,29 +40,17 @@ public class SpawnManager : MonoBehaviour
     {
         int animalIndex = Random.Range(0, animalPrefabs.Length);
         Vector3 spawnPos = new Vector3(-sideSpawnX, 0, Random.Range(sideSpawnMinZ, sideSpawnMaxZ));
-<<<<<<< HEAD
         Vector3 rotation = new Vector3(0, 90, 0);
-        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
-
-    }
-        
-=======
-        Vector3 rotation = new Vector3(-14, -270, 2);
         Instantiate(animalPrefabs[animalIndex], spawnPos, Quaternion.Euler(rotation));
-    }
 
+    }
     void SpawnRightAnimal()
     {
         int animalIndex = Random.Range(0, animalPrefabs.Length);
         Vector3 spawnPos = new Vector3(sideSpawnX, 0, Random.Range(sideSpawnMinZ, sideSpawnMaxZ));
-        Vector3 rotation = new Vector3(39, 270, 7);
+        Vector3 rotation = new Vector3(0, 90, 7);
         Instantiate(animalPrefabs[animalIndex], spawnPos, Quaternion.Euler(rotation));
-        {
-
-        }
+    }
 
     }
-}
-        
-   
->>>>>>> a8a4e9e7bc76ad8b8d714152a3b010a97f1a6a50
+    
